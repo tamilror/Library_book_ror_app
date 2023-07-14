77d971b975f6633e05b49ef0f6e36ef9040538d9
Renuka Kanagaraju <renukakanagaraju@Renukas-MacBook-Pro.local>
2023-07-14 23:34:31 +0530

    added project

--- test/controllers/api/v1/libraries_controller_test.rb
+++ test/controllers/api/v1/libraries_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class Api::V1::LibrariesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    test 'should get library with books' do
+    library = libraries(:one)
+    get api_library_url(library), as: :json
+
+    assert_response :success
+    assert_equal library.id, response.parsed_body['library']['id']
+    assert_equal library.books.count, response.parsed_body['books'].size
+  end
 end
